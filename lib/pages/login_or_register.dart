570b09503441d48d043334677b22ca56690ec704
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-15 08:36:26 +0700

    finish: source code

---
+++ lib/pages/login_or_register.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:travel/pages/login_page.dart';
+import 'package:travel/pages/register_page.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  const LoginOrRegister({Key? key}) : super(key: key);
+
+  @override
+  State<LoginOrRegister> createState() => _LoginOrRegisterState();
+}
+
+class _LoginOrRegisterState extends State<LoginOrRegister> {
+  // initially show login page
+  bool showLoginPage = true;
+
+  // toggle between login and register page
+  void togglePage(){
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(showLoginPage){
+      return LoginPage(onTap: togglePage);
+    }else{
+      return RegisterPage(onTap: togglePage);
+    }
+  }
+}
